3fc567e5993945de6584ace94ac300509412671b
Pete Hudgins <pete.hudgins@gmail.com>
2012-09-21 13:18:28 -0400

    Rescue TypeError

--- lib/learning_registry/resource_data.rb
+++ lib/learning_registry/resource_data.rb
@@ -47,22 +47,26 @@ class LearningRegistry::ResourceData
   end
 
   def self.initialize_from_xml_string(xml)
-    parsed = Nokogiri::XML(xml)
-    if parsed.xml?
-      parsed.remove_namespaces!
-      identifier = parsed.at("//identifier").children.first.to_s if parsed.at("//identifier")
-      title = parsed.at("//title").children.first.to_s if parsed.at("//title")
-      description = parsed.at("//description").children.first.to_s if parsed.at("//description")
-      language = parsed.at("//language").children.first.to_s if parsed.at("//language")
-      education_level = parsed.at("//educationLevel").children.first.to_s if parsed.at("//educationLevel")
-      audience = parsed.at("//audience").children.first.to_s if parsed.at("//audience")
-      subject = parsed.at("//subject").children.first.to_s if parsed.at("//subject")
-      publisher = parsed.at("//publisher").children.first.to_s if parsed.at("//publisher")
-      type = parsed.at("//type").children.first.to_s if parsed.at("//type")
+    begin
+      parsed = Nokogiri::XML(xml)
+      if parsed.xml?
+        parsed.remove_namespaces!
+        identifier = parsed.at("//identifier").children.first.to_s if parsed.at("//identifier")
+        title = parsed.at("//title").children.first.to_s if parsed.at("//title")
+        description = parsed.at("//description").children.first.to_s if parsed.at("//description")
+        language = parsed.at("//language").children.first.to_s if parsed.at("//language")
+        education_level = parsed.at("//educationLevel").children.first.to_s if parsed.at("//educationLevel")
+        audience = parsed.at("//audience").children.first.to_s if parsed.at("//audience")
+        subject = parsed.at("//subject").children.first.to_s if parsed.at("//subject")
+        publisher = parsed.at("//publisher").children.first.to_s if parsed.at("//publisher")
+        type = parsed.at("//type").children.first.to_s if parsed.at("//type")
+      end
+      new(identifier: identifier, title: title, description: description, language: language,
+          education_level: education_level, audience: audience, subject: subject, type: type,
+          publisher: publisher)
+    rescue TypeError
+      new()
     end
-    new(identifier: identifier, title: title, description: description, language: language,
-        education_level: education_level, audience: audience, subject: subject, type: type,
-        publisher: publisher)
   end
 
 end
